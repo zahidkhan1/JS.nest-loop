d9390991b26e5d2d83ee9a679c5458e928205b74
Zahid khan <zahidkhan43743@gmail.com>
2025-03-03 16:28:21 +0500

    JS-nest loop

---
+++ App.js
@@ -0,0 +1,24 @@
+// let firstName = ["khan","wali","haji","ok"];
+// let LastName = ["muhammad","khan","gul","jananb"];
+// let fullName = [];
+// for(i=0;i<firstName;i++){
+//     for(j=0;j<LastName;j++)
+// }
+
+// let i=prompt("enter table");
+// for(i;i<=10;i++){
+//     console.log(i + "*" + "2" + "= " + i *2);
+    
+// }
+
+let color=["blue","red","green","yellow"];
+let obj=["car","airline","jeep"];
+let fullobj=[];
+for(i=0;i<color.length;i++){
+    for(j=0;j<obj.length;j++){
+    fullobj.push(color[i]+obj[j]);
+    
+    console.log(fullobj)
+    }
+
+}
